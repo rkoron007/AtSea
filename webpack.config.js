6db14ecb1df54b027044efdd0c16722409be8b3a
Rose <rkoron007@gmail.com>
2018-04-13 15:28:18 -0700

    Setting up Final Heroku Push

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,49 @@
-var path = require('path');
+var path = require("path");
+var webpack = require("webpack");
 
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
+
+// include plugins config
 module.exports = {
-  entry: './frontend/entry.jsx',
+  context: __dirname,
+  entry: "./frontend/<name of entry file>",
   output: {
-    filename: 'app/assets/javascripts/bundle.js',
+      filename: 'app/assets/javascripts/bundle.js',
+    filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['env', 'react']
+          presets: ['react', 'env']
         }
       }
     ]
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['.js', '.jsx', '*']
+    extensions: [".js", ".jsx", "*"]
   }
 };
