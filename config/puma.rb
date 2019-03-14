84317a014af7a6fe6b96c4b25a9f078d69b4e72b
rkoron <rkoron007@gmail.com>
2019-03-14 09:19:05 -0700

    Change Worker Count

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
@@ -21,6 +22,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
